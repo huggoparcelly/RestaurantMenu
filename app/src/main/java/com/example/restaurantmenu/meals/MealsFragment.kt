4de2dccfcfbd659831f3f293f574208c0b2a266e
Huggo Parcelly <h.parcelly@gmail.com>
2025-02-25 22:13:26 -0300

    start project

---
+++ app/src/main/java/com/example/restaurantmenu/meals/MealsFragment.kt
@@ -0,0 +1,31 @@
+package com.example.restaurantmenu.meals
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.restaurantmenu.R
+
+
+class MealsFragment : Fragment() {
+
+    private lateinit var mRecyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_meals, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+}
